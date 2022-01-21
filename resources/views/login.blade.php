ba0501905aad7e9ee8cffdabcd46ddf68c19df12
ArthurMukashev <arthur.mukasev@gmail.com>
2022-01-21 11:21:39 +0500

    вход не по email, а по логину (input type="text")

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -24,7 +24,7 @@
                       <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
                     </svg>
                 </span>
-                <input type="email" name="email" class="form-control" placeholder="Логин">
+                <input type="text" name="email" class="form-control" placeholder="Логин">
             </div>
             <div class="input-group mb-3">
                 <span class="input-group-text">
